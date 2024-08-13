2c810c1cd776cdc547f6836c487ecedd964a9177
raflinaufal <raflinaufalalief204@gmail.com>
2024-08-14 06:34:12 +0700

    create register page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-document
-  .querySelector(".custom-toggler")
-  .addEventListener("click", function () {
-    const navbarNav = document.getElementById("navbarNav");
-    if (this.classList.contains("collapsed")) {
-      this.classList.remove("collapsed"); // Menghapus kelas collapsed (membuka navbar)
-      navbarNav.style.display = "block"; // Menampilkan navbar
-    } else {
-      this.classList.add("collapsed"); // Menambahkan kelas collapsed (menutup navbar)
-      navbarNav.style.display = "none"; // Menyembunyikan navbar
-    }
+// Swiper initialization
+const swiper = new Swiper(".swiper-container", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 2.5,
+  spaceBetween: 10,
+});
+
+// Smooth scroll function
+function smoothScroll(target) {
+  document.querySelector(target).scrollIntoView({ behavior: "smooth" });
+}
+
+// Event listeners for navbar links
+document.querySelectorAll(".navbar-nav .nav-link").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute("href");
+    smoothScroll(targetId);
   });
+});
